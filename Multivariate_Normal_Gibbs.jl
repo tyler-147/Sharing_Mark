6f403533cfc54bbb14389d1ea44b1e02b9a96598
bayesthm <mark.j.bognanni@gmail.com>
2019-10-08 14:37:45 -0400

    shuffle order of functions

--- Multivariate_Normal_Gibbs.jl
+++ Multivariate_Normal_Gibbs.jl
@@ -13,6 +13,18 @@ Random.seed!(8675309)
 # Function
 # -----------------------------------------------------------------------------
 
+function setjinds( j::Int, sub_len::Int, full_len::Int )
+
+    #indices of j-th subvector
+    start = Int((j - 1)*sub_len + 1)
+    stop  = Int(j*sub_len)
+
+    jinds    = start:stop                   # UnitRange
+    notjinds = setdiff(1:full_len, jinds)   # Vector of Ints
+
+    return (jinds, notjinds)
+end
+
 function condcov( S::Matrix{<:AbstractFloat}, 
                   jinds::UnitRange{Int}, 
                   notjinds::Vector{Int} )
@@ -31,18 +43,6 @@ function condcov( S::Matrix{<:AbstractFloat},
 
     return (Σ, Σ12_invΣ22)
 
-end
-
-function setjinds( j::Int, sub_len::Int, full_len::Int )
-
-    #indices of j-th subvector
-    start = Int((j - 1)*sub_len + 1)
-    stop  = Int(j*sub_len)
-
-    jinds    = start:stop                   # UnitRange
-    notjinds = setdiff(1:full_len, jinds)   # Vector of Ints
-
-    return (jinds, notjinds)
 end
 
 # single draw
