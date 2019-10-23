310132d7b206165bd81ad3b798ec56ef341bcd00
bayesthm <mark.j.bognanni@gmail.com>
2019-10-22 21:56:45 -0400

    slight generalization of Integer behavior

--- Mv_Gibbs/helpers_indexing.jl
+++ Mv_Gibbs/helpers_indexing.jl
@@ -1,11 +1,11 @@
-function setjinds( j::Int, sub_len::Int, full_len::Int )
+function setjinds( j::Integer, sub_len::Integer, full_len::Integer )
 
     #indices of j-th subvector
-    start = Int((j - 1)*sub_len + 1)
-    stop  = Int(j*sub_len)
+    start = Integer((j - 1)*sub_len + 1)
+    stop  = Integer(j*sub_len)
 
     jinds    = start:stop                   # UnitRange
-    notjinds = setdiff(1:full_len, jinds)   # Vector of Ints
+    notjinds = setdiff(1:full_len, jinds)   # Vector of Integers
 
     return (jinds, notjinds)
 end
